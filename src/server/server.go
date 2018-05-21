3445b00ad3ffab6a69843da891236d941ba69bee
Prash <prashantnaicker@gmail.com>
2018-05-21 16:23:54 +1200

    New Start

--- src/server/server.go
+++ src/server/server.go
@@ -1,53 +1,25 @@
-package main
+package server
 
 import (
     "fmt"
+    "net"
     "net/http"
-
-    "golang.org/x/crypto/acme/autocert"
 )
 
 // Entry.
-func Start() chan error {
-	errc := make(chan error)
-	httpsMux := http.NewServeMux()
-	httpsMux.HandleFunc("/", requestHandler)
+func Start() error {
+    l, err := net.Listen("tcp", ":8081")
+    if err != nil { fmt.Printf("%v", err); return err }
 
-    acmeMgr := autocert.Manager{
-		Prompt:     autocert.AcceptTOS,
-		HostPolicy: autocert.HostWhitelist("127.0.0.1"),
-		Cache:      autocert.DirCache("autocert-cache"),
-	}
-	go func() {
-		errc <- http.ListenAndServe(":http", acmeMgr.HTTPHandler(nil))
-	}()
+    for {
+        conn, err := l.Accept()
+        if err != nil { fmt.Printf("%v", err); continue }
 
-	tlsConfig := &tls.Config{
-		MinVersion:               tls.VersionTLS12,
-		CurvePreferences:         []tls.CurveID{tls.X25519, tls.CurveP521, tls.CurveP384, tls.CurveP256},
-		GetCertificate:           acmeMgr.GetCertificate,
-		PreferServerCipherSuites: true,
-	}
-
-	httpsServer := &http.Server{
-		Addr:         ":443",
-		Handler:      httpsMux,
-		TLSConfig:    tlsConfig,
-		ReadTimeout:  30 * time.Second,
-		WriteTimeout: 30 * time.Second,
-		ErrorLog:     log.New(ioutil.Discard, "", 0),
-		TLSNextProto: nil,
-	}
-
-	go func() {
-		errc <- httpsServer.ListenAndServe("", "")
-	}()
-
-	return errc
+        go handleConnection(conn)
+    }
 }
 
-func requestHandler(w http.ResponseWriter, r *http.Request) {
-    SetGeneralHeaders(w)
+func handleConnection(conn net.Conn) {
     fmt.Printf("works")
 }
 
